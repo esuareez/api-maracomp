a33a3ad8b78e686254ee2f96c71928f2b30289b8
Eliam Pimentel <eliamps07@outlook.com>
2023-06-26 13:41:55 -0400

    dispach prop fix

--- src/dispach/dispach.service.ts
+++ src/dispach/dispach.service.ts
@@ -9,56 +9,60 @@ import { StoreService } from 'src/store/store.service';
 
 @Injectable()
 export class DispachService {
-    constructor(@InjectModel(Dispach.name) private readonly dispachModel : Model<Dispach>,
-    private readonly inventoryMovementService : InventorymovementService,
-    private readonly componentService : ComponentService,
-    private readonly storeService : StoreService){}
+  constructor(
+    @InjectModel(Dispach.name) private readonly dispachModel: Model<Dispach>,
+    private readonly inventoryMovementService: InventorymovementService,
+    private readonly componentService: ComponentService,
+    private readonly storeService: StoreService,
+  ) {}
 
-    async create(dispach : any) {
-        const createdDispach = new this.dispachModel(dispach);
-        createdDispach.code = await this.generateCode();
-        createdDispach.date = new Date();
-      
-        for (const detailItem of dispach.detail) {
-          const { componentId, quantity, storeId } = detailItem;
-      
-          const createdStore = await this.storeService.findById(storeId);
-          const createdComponent = await this.componentService.findById(componentId);
-      
-          for (const store of createdComponent.store) {
-            console.log(`${store.store} --- ${storeId}`)
-            if (store.store === storeId) {
-              store.balance -= quantity;
-              await this.componentService.update(componentId, createdComponent);
-              break; // No es necesario seguir iterando después de encontrar la tienda correspondiente
-            }
-          }
-      
-          const inventoryMovement = {
-            date: new Date(),
-            idStore: storeId,
-            type: 'SALIDA',
-            detail: [
-              {
-                idComponent: componentId,
-                quantity,
-                unit: createdComponent.unit,
-              },
-            ],
-          };
-      
-          await this.inventoryMovementService.create(inventoryMovement);
+  async create(dispach: Dispach) {
+    const createdDispach = new this.dispachModel(dispach);
+    createdDispach.code = await this.generateCode();
+    createdDispach.date = new Date();
+
+    for (const detailItem of dispach.detail) {
+      const { componentId, quantity, storeId } = detailItem;
+
+      const createdStore = await this.storeService.findById(storeId);
+      const createdComponent = await this.componentService.findById(
+        componentId,
+      );
+
+      for (const store of createdComponent.store) {
+        console.log(`${store.store} --- ${storeId}`);
+        if (store.store === storeId) {
+          store.balance -= quantity;
+          await this.componentService.update(componentId, createdComponent);
+          break; // No es necesario seguir iterando después de encontrar la tienda correspondiente
         }
-      
-        return await createdDispach.save();
       }
 
-    async findAll(){
-        return await this.dispachModel.find().exec();
+      const inventoryMovement = {
+        date: new Date(),
+        idStore: storeId,
+        type: 'SALIDA',
+        detail: [
+          {
+            idComponent: componentId,
+            quantity,
+            unit: createdComponent.unit,
+          },
+        ],
+      };
+
+      await this.inventoryMovementService.create(inventoryMovement);
     }
 
-    async generateCode(){
-        const lastCode = await this.findAll();
-        return lastCode.length + 1;
-    }
+    return await createdDispach.save();
+  }
+
+  async findAll() {
+    return await this.dispachModel.find().exec();
+  }
+
+  async generateCode() {
+    const lastCode = await this.findAll();
+    return lastCode.length + 1;
+  }
 }
--- src/dispach/schema/dispach.schema.ts
+++ src/dispach/schema/dispach.schema.ts
@@ -1,16 +1,19 @@
-import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
 export class Dispach {
-    @Prop()
-    code?: Number
-    @Prop()
-    date?: Date
-    detail:[{
-        componentId: string
-        quantity: number
-        storeId: string
-    }]
+  @Prop()
+  code?: Number;
+  @Prop()
+  date?: Date;
+  @Prop()
+  detail: [
+    {
+      componentId: string;
+      quantity: number;
+      storeId: string;
+    },
+  ];
 }
 
 export const DispachSchema = SchemaFactory.createForClass(Dispach);
