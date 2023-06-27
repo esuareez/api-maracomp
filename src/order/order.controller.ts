4bf3eb67e397740b1db99e0fed354696f3d72c49
Eliam Pimentel <eliamps07@outlook.com>
2023-06-27 00:35:57 -0400

    delete order and detailorder

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,13 +1,18 @@
 import { Controller } from '@nestjs/common';
-import { Delete } from '@nestjs/common';
+import { Delete, Get } from '@nestjs/common';
 import { OrderService } from './order.service';
 
 @Controller('order')
 export class OrderController {
-    constructor(private readonly orderService : OrderService) {}
+  constructor(private readonly orderService: OrderService) {}
 
-    @Delete()
-    async deleteAll(){
-        return this.orderService.deleteAll();
-    }
+  @Delete()
+  async deleteAll() {
+    return this.orderService.deleteAll();
+  }
+
+  @Get()
+  async findAll() {
+    return this.orderService.findAll();
+  }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -6,55 +6,75 @@ import { IsNotEmpty } from 'class-validator';
 
 @Injectable()
 export class OrderService {
-    constructor(
-        @InjectModel(Order.name) private orderModel: Model<Order>
-    ){}
+  constructor(@InjectModel(Order.name) private orderModel: Model<Order>) {}
 
-    async create(order: Order){
-        const newOrder = new this.orderModel(order);
-        return await newOrder.save();
+  async create(order: Order) {
+    const newOrder = new this.orderModel(order);
+    return await newOrder.save();
+  }
+
+  async findAll() {
+    return await this.orderModel.find().exec();
+  }
+
+  async findByCodeAndDate(code: Number, date: Date) {
+    return await this.orderModel.findOne({ code, date }).exec();
+  }
+
+  async findOrderByDateRequestAndSupplier(
+    date: Date,
+    orderRequestId: string,
+    supplierId: string,
+  ) {
+    return await this.orderModel
+      .findOne({ date, orderRequestId, supplierId })
+      .exec();
+  }
+
+  async findByCode(code: Number) {
+    return await this.orderModel.findOne({ code }).exec();
+  }
+
+  async generateCode() {
+    const lastOrder = await this.findAll();
+    return lastOrder.length + 1;
+  }
+
+  async findOrderBySupplierOrderRequestAndDate(
+    supplierCode: string,
+    orderRequestCode: string,
+    date: Date,
+  ) {
+    return await this.orderModel
+      .findOne({ supplierCode, orderRequestCode, date })
+      .exec();
+  }
+
+  async updateTotalByCode(code: Number, order: Order) {
+    let _order = await this.orderModel.findOne({ code }).exec();
+    if (_order !== null) {
+      _order.total = order.total;
     }
-    
-    async findAll(){
-        return await this.orderModel.find().exec();
-    }
+    return await this.orderModel
+      .findByIdAndUpdate(_order._id, _order, { new: true })
+      .exec();
+  }
 
-    async findByCodeAndDate(code: Number, date: Date){
-        return await this.orderModel.findOne({code, date}).exec();
-    }
+  async update(id: string, order: Order) {
+    return await this.orderModel
+      .findByIdAndUpdate(id, order, { new: true })
+      .exec();
+  }
 
-    async findOrderByDateRequestAndSupplier(date: Date, orderRequestId: string, supplierId: string){
-        return await this.orderModel.findOne({date, orderRequestId, supplierId}).exec();
-    }
+  async deleteAll() {
+    return await this.orderModel.deleteMany({}).exec();
+  }
 
-    async findByCode(code: Number){
-        return await this.orderModel.findOne({code}).exec();
-    }
+  async delete(id: string) {
+    return await this.orderModel.findByIdAndDelete(id).exec();
+  }
 
-    async generateCode(){
-        const lastOrder = await this.findAll()
-        return lastOrder.length + 1 
-    }
-
-    async findOrderBySupplierOrderRequestAndDate(supplierCode: string, orderRequestCode: string, date: Date){
-        return await this.orderModel.findOne({supplierCode, orderRequestCode, date}).exec();
-    }
-
-    async updateTotalByCode(code: Number, order: Order){
-        let _order = await this.orderModel.findOne({code}).exec();
-        if(_order !== null){
-            _order.total = order.total;
-        }
-        return await this.orderModel.findByIdAndUpdate(_order._id, _order, {new: true}).exec();
-    }
-
-    async update(id: string, order: Order){
-        return await this.orderModel.findByIdAndUpdate(id, order, {new: true}).exec();
-    }
-
-    async deleteAll(){
-        return await this.orderModel.deleteMany({}).exec();
-    }
-
-
+  async deleteByCode(code: Number) {
+    return await this.orderModel.deleteOne({ code }).exec();
+  }
 }
